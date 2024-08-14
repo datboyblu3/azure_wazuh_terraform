98e9cf2efadcc1e90fb39149ca3b1adc0be00048
datboyblu3 <datboyblu3@protonmail.com>
2024-08-14 08:15:36 -0700

    fixed redudnant SSH code

--- 3-compute.tf
+++ 3-compute.tf
@@ -16,7 +16,6 @@ data "azurerm_key_vault_secret" "ssh_keys" {
   key_vault_id = data.azurerm_key_vault.kv.id
 }
 
-
 ####################################################################
 # Wazuh Indexer Compute Instance
 ####################################################################
@@ -30,17 +29,9 @@ resource "azurerm_linux_virtual_machine" "az-indexer_1" {
     azurerm_network_interface.az-wazuh-nic.id
   ]
 
-  # Get a list of secrets in the key vault
-  /*data "azurerm_key_vault_secret" "ssh_key" {
-    for_each     = toset(data.azurerm_key_vault_secrets.ssh_key.names)
-    name         = each.key
-    key_vault_id = data.azurerm_key_vault.existing.id
-
-  }*/
-
   admin_ssh_key {
     username   = "datboyblu3"                                          
-    public_key = data.azurerm_key_vault_secrets.ssh_keys.public_key_openssh
+    public_key = data.azurerm_key_vault_secret.ssh_keys.value
   }
 
   os_disk {
@@ -58,8 +49,24 @@ resource "azurerm_linux_virtual_machine" "az-indexer_1" {
   tags = {
     environment = "dev"
   }
-}
 
+  disable_password_authentication = false
+
+  /*provisioner "file" {
+    source = "ANSIBLE SERVER INSTALL SCRIPT"
+    destination = "DESTINATION LOCATION FOR SERVER INSTALL SCRIPT"
+
+    connection {
+        host        = coalesce()
+        agent       = true 
+        type        = "ssh"
+        user        = "adminuser"
+        private_key = tls_private_key.indexer_key.private_key_pem
+
+
+    }
+  }*/
+}
 
 ####################################################################
 # Wazuh Server Compute Instance
@@ -74,17 +81,9 @@ resource "azurerm_linux_virtual_machine" "az-server_1" {
     azurerm_network_interface.az-wazuh-nic.id
   ]
 
-  # Get a list of secrets in the key vault
-  /*data "azurerm_key_vault_secret" "ssh_key" {
-    for_each     = toset(data.azurerm_key_vault_secrets.ssh_key.names)
-    name         = each.key
-    key_vault_id = data.azurerm_key_vault.existing.id
-
-  }*/
-
   admin_ssh_key {
     username   = "datboyblu3"
-    public_key = data.azurerm_key_vault_secrets.ssh_keys.public_key_openssh #PULL INDEX KEY FROM AZURE KEY VAULT
+    public_key = data.azurerm_key_vault_secret.ssh_keys.value
   }
 
   os_disk {
@@ -102,6 +101,8 @@ resource "azurerm_linux_virtual_machine" "az-server_1" {
   tags = {
     environment = "dev"
   }
+
+  disable_password_authentication = false
 
   /*provisioner "file" {
     source = "ANSIBLE SERVER INSTALL SCRIPT"
@@ -128,22 +129,14 @@ resource "azurerm_linux_virtual_machine" "az-dashboard_1" {
   resource_group_name = azurerm_resource_group.az-wazuh-grp.name
   location            = azurerm_resource_group.az-wazuh-grp.location
   size                = "Standard_B1s"
-  admin_username      = "dash" #CHANGEME
+  admin_username      = "dash"
   network_interface_ids = [
     azurerm_network_interface.az-wazuh-nic.id
   ]
 
-  # Get a list of secrets in the key vault
-  /*data "azurerm_key_vault_secret" "ssh_key" {
-    for_each     = toset(data.azurerm_key_vault_secrets.ssh_key.names)
-    name         = each.key
-    key_vault_id = data.azurerm_key_vault.existing.id
-
-  }*/
-
   admin_ssh_key {
     username   = "datboyblu3"
-    public_key = data.azurerm_key_vault_secrets.ssh_keys.public_key_openssh #PULL INDEX KEY FROM AZURE KEY VAULT
+    public_key = data.azurerm_key_vault_secret.ssh_keys.value
   }
 
   os_disk {
@@ -161,6 +154,8 @@ resource "azurerm_linux_virtual_machine" "az-dashboard_1" {
   tags = {
     environment = "dev"
   }
+
+  disable_password_authentication = false
 
   /*provisioner "file" {
     source = "ANSIBLE DASHBOARD INSTALL SCRIPT"
--- temp/variables.tf
+++ temp/variables.tf
@@ -1,7 +1,3 @@
-provider "azurerm" {
-  features {}
-}
-
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
